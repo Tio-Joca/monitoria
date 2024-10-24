dcd3290ab269675d5a0669cbab1d940f308436f4
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2024-10-23 21:52:00 -0300

    Atualização no pacote repetição

--- monitoriaJava/src/repeticao/Enquanto.java
+++ monitoriaJava/src/repeticao/Enquanto.java
@@ -5,33 +5,82 @@ import java.util.Scanner;
 public class Enquanto {
     public static void main (String[] args) {
         Scanner teclado;
-        int num;
+        int numUm, numDois;
+        int fatorial, countUm, countDois, base, exp, potencia;
         boolean verificador;
 
         teclado = new Scanner (System.in);
 
-        System.out.print("Digite um valor inteiro positivo maior que 1: ");
-        num = Math.abs(teclado.nextInt());
+        System.out.println("Verificacao de numero primo\n");
 
-        while (num <= 1)    {
+        System.out.print("Digite um valor inteiro positivo maior que 1: ");
+        numUm = Math.abs(teclado.nextInt());
+
+        while (numUm <= 1)    {
             System.out.print("Entrada invalida.\nDigite um valor inteiro positivo maior que 1: ");
-            num = Math.abs(teclado.nextInt());
+            numUm = Math.abs(teclado.nextInt());
         }
 
         verificador = false;
 
-        for (int divisor = 2; divisor < num && !(verificador); divisor++)    {
-            if (num % divisor == 0) {
+        for (int divisor = 2; divisor < numUm && !(verificador); divisor++)    {
+            if (numUm % divisor == 0) {
                 verificador = true;
             }
         }
 
         if (verificador)    {
-            System.out.println("O numero inteiro " + num + " nao e primo");
+            System.out.println("O numero inteiro " + numUm + " nao e primo\n");
         }   else    {
-            System.out.println("O numero inteiro " + num + " e primo");
+            System.out.println("O numero inteiro " + numUm + " e primo\n");
         }
 
+        System.out.println("Operacao de fatorial\n");
+
+        System.out.print("Digite um numero inteiro referente ao fatorial que voce deseja obter: ");
+        numDois = teclado.nextInt();
+
+        if (numDois == 0)  {
+            fatorial = 0;
+        }   else if (numDois == 1)  {
+            fatorial = 1;
+        }   else    {
+            fatorial = 2;
+            countUm = 3;
+            while (countUm <= numDois)  {
+                fatorial = fatorial * countUm;
+                countUm++;
+            }
+        }
+
+        System.out.println("O fatorial de " + numDois + " e " + fatorial + "!\n");
+
+        System.out.println("Operacao de potencia\n");
+
+        System.out.print("Digite o valor inteiro referente a base da potencia: ");
+        base = teclado.nextInt();
+
+        System.out.print("Digite o valor inteiro referente ao expoente da potencia ");
+        exp = teclado.nextInt();
+
+        switch (exp)    {
+            case 0:
+                potencia = 0;
+                break;
+            case 1:
+                potencia = base;
+                break;
+            default:
+                countDois = 2;
+                potencia = base;
+                while (countDois < exp + 1) {
+                    potencia = potencia * base;
+                    countDois++;
+                }
+                break;
+        }
+
+        System.out.println("O potencia de base " + base + " e expoente " + exp + " e igual a " + potencia + "!\n");
 
         teclado.close();
     }
