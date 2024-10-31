ecd1045f25904e63a7a1359bbe45744f0bcac6a9
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2024-10-31 11:56:46 -0300

    Restruturação do projeto monitoriaJava

--- monitoriaJava/src/objetosSimples/Carro.java
+++
@@ -1,9 +0,0 @@
-package objetosSimples;
-
-public class Carro {
-    private String nome;
-    private String modelo;
-    private int potencia;
-    private double torque;
-    
-}
--- monitoriaJava/src/saidaEntrada/Saida.java
+++
@@ -1,7 +0,0 @@
-package saidaEntrada;
-
-public class Saida {
-    public static void main(String[] args) {
-        System.out.println("Ola mundo!");
-    }
-}
