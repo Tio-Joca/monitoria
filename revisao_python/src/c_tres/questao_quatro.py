67da5614d93ff970f68fc6bb3be2f40b8d671d72
Joaquim Lucas Pessoa da Silva <joaquim.pessoa@tutamail.com>
2024-11-26 17:52:13 -0300

    Criação do diretório revisao_python
    Este diretório foi criado para abordar os exercícios de programação que irei utilizar numa revisão de conceitos de programação em Python para uma turma de laboratório de programação da minha faculdade.

---
+++ revisao_python/src/c_tres/questao_quatro.py
@@ -0,0 +1,22 @@
+i : int
+
+for i in range(1, 11):
+    if (i == 10):
+        print(str(i) + '\n')
+    else:
+        print(i)
+
+for i in range(11):
+    if (i == 10):
+        print(str(i) + '\n')
+    else:
+        print(i)
+
+for i in range(1, 10):
+    if (i == 9):
+        print(str(i) + '\n')
+    else:
+        print(i)
+
+for i in range(10):
+    print(i)
---
+++ revisao_python/src/c_tres/questao_um.py
@@ -0,0 +1,11 @@
+x : float
+y : int
+
+x = 9 / 3
+y = 9 // 3
+
+print(x)
+print(str(y) + '\n')
+
+print(type(x))
+print(type(y))
